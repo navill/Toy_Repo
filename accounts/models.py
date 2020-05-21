fb6822b18baa10c5ca6a28a6dbf476df4f2766c5
jihoon <blue_jihoon@naver.com>
2020-05-21 17:48:42 +0900

    foreignkey -> onetoonefield 변경

--- accounts/models.py
+++ accounts/models.py
@@ -4,7 +4,6 @@ from django.db import models
 
 # Create your models here.
 class Profile(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     phone = models.CharField(max_length=14, db_index=True)
     address = models.CharField(max_length=200)
-
