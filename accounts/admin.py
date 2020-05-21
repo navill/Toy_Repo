acfdee804e1b259cf29cc7fa3eb13b1b5c33e65d
jihoon <blue_jihoon@naver.com>
2020-05-21 17:40:34 +0900

    admin 설정

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from posts.models import Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['user', 'title', 'created', 'updated']
+
+
+admin.site.register(Post, PostAdmin)
--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from accounts.models import Profile
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'phone', 'address']
+
+
+admin.site.register(Profile, ProfileAdmin)
